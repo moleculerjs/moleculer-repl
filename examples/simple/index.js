43392bc860b80d8beaca1c662392fa201857b951
Icebob <mereg.norbert@gmail.com>
2018-01-29 16:26:56 +0100

    add custom command support

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -7,7 +7,30 @@ let REPL 				= require("../../src");
 let broker = new ServiceBroker({
 	nodeID: "repl-" + process.pid,
 	transporter: "NATS",
-	logger: console
+	logger: console,
+
+	// Custom REPL command
+	replCommands: [
+		{
+			command: "hello <name>",
+			description: "Call the greeter.hello service with name",
+			alias: "hi",
+			options: [
+				{ option: "-u, --uppercase", description: "Uppercase the name" }
+			],
+			types: {
+				string: ["name"],
+				boolean: ["u", "uppercase"]
+			},
+			//parse(command, args) {},
+			//validate(args) {},
+			//help(args) {},
+			allowUnknownOptions: true,
+			action(args) {
+				return broker.call("greeter.hello", { name: args.name }).then(console.log);
+			}
+		}
+	]	
 });
 
 broker.createService({
@@ -33,4 +56,4 @@ broker.createService({
 	}
 });
 
-broker.start().then(() => REPL(broker));
+broker.start().then(() => REPL(broker, broker.options.replCommands));
--- src/index.js
+++ src/index.js
@@ -631,6 +631,38 @@ function REPL(broker, customCommands /* TODO */) {
 			done();
 		});			
 
+	// Register custom commands
+	if (Array.isArray(customCommands)) {
+		customCommands.forEach(def => {
+			let cmd = vorpal.command(def.command, def.description);
+			if (def.alias)
+				cmd.alias(def.alias);
+
+			if (Array.isArray(def.options)) {
+				def.options.forEach(opt => cmd.option(opt.option, opt.description, opt.autocomplete));				
+			}
+
+			if (def.parse)
+				cmd.parse(def.parse);
+
+			if (def.types)
+				cmd.types(def.types);
+
+			if (def.help)
+				cmd.help(def.help);
+
+			if (def.validate)
+				cmd.validate(def.validate);
+
+			if (def.allowUnknownOptions)
+				cmd.allowUnknownOptions();
+
+			cmd.action(def.action);
+
+			if (def.cancel)
+				cmd.cancel(def.cancel);
+		});
+	}
 	
 	// Start REPL
 	vorpal

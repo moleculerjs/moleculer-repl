c8c0099ed00e19988f93efaef0f60e51dc285f0c
Icebob <mereg.norbert@gmail.com>
2018-02-28 11:07:23 +0100

    change year

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
  * moleculer-repl
- * Copyright (c) 2017 MoleculerJS (https://github.com/moleculerjs/moleculer-repl)
+ * Copyright (c) 2018 MoleculerJS (https://github.com/moleculerjs/moleculer-repl)
  * MIT Licensed
  */
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*eslint-disable no-console*/
 /*
  * moleculer-repl
- * Copyright (c) 2017 MoleculerJS (https://github.com/moleculerjs/moleculer-repl)
+ * Copyright (c) 2018 MoleculerJS (https://github.com/moleculerjs/moleculer-repl)
  * MIT Licensed
  */
 
